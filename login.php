1ee73a5b9487e778ad76b5a57eb562dccedaf406
Shruti-Sundaram <72123949+Shruti-Sundaram@users.noreply.github.com>
2021-04-09 09:39:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,8 +11,10 @@
 <form method="post" action="login.php">
  <div class="input-group">
     Username: <input type="text" name="username" required><br><br>
-    Password: <input type="password" name="pw1" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+   <a href="index.php">
     <button type="submit" name="login" class="btn">Login</button>
+     </a>
  </div>
 <p>
 Not registered yet? <a href="register.php">Register
